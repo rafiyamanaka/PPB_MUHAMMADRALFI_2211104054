ba71d4e4837a3b3879281597527cecdb2f994402
Ralfi <rafiyamanaka32@gmail.com>
2024-10-21 22:19:16 +0700

    Pengumpulan TP 06_Interaksi pengguna

--- 05_Antarmuka_Pengguna_Lanjutan/GUIDED/guided5/lib/main.dart
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/guided5/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guided5/customScroll.dart';
 // import 'package:guided5/customScroll.dart';
 // import 'package:guided5/flex_exp.dart';
 import 'package:guided5/jenis_list.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       ),
       // home: const customScrollview(),
       // home: const flexScreen(),
-      home: const listScreen(),
+      home: const customScrollview(),
     );
   }
 }
