11fbbd2ccd04101fd509650c547f56cdd3b5044b
Ralfi <rafiyamanaka32@gmail.com>
2024-11-13 10:22:19 +0700

    Guided Praktikum 9

---
+++ 09_API_Perangkat_Keras/GUIDED/prak9/lib/cam.dart
@@ -0,0 +1,77 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:prak9/display_screen.dart';
+
+class camera_screen extends StatefulWidget {
+  const camera_screen({super.key});
+
+  @override
+  State<camera_screen> createState() => _camera_screenState();
+}
+
+class _camera_screenState extends State<camera_screen> {
+  late CameraController _controller;
+  Future<void>? _initializeControllerFuture;
+
+  Future<void>? _initializeCamera() async {
+    final cameras = await availableCameras();
+    final firstCamera = cameras.first;
+
+    _controller = CameraController(firstCamera, ResolutionPreset.high);
+    _initializeControllerFuture = _controller.initialize();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _initializeCamera();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera Flutter'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: FutureBuilder(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller);
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => DisplayScreen(
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
